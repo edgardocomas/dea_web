1349467642bae6a7767b0179c5de387f86d7a4ac
Comas <ecomas@ad.citedef.gob.ar>
2023-03-07 18:32:29 -0300

    v2

---
+++ JS/process.js
@@ -0,0 +1,28 @@
+
+
+function director(dato) {
+    switch (dato) {
+        case 1:
+          return "Dr. Ing. Edgardo Comas" 
+          break;
+        case 2:
+          return "Ing. Cristian Bruña" 
+          break;
+        case 3:
+          return "Ing. Sergio Lara" 
+          break;
+        case 4:
+          return "Ing. Cristian Arrieta" 
+          break;
+        case 5:
+          return "Ing. Norberto Digiovanni" 
+          break;
+        case 6:
+          return "Ing. Gustavo Moyano" 
+          break;
+        case 7:
+          return "Ing. Darío Yokhdar" 
+          break;
+      }
+}
+
